5f955921fa4d6e8c4a89059221e31cb16aed163c
JEISON_RODRIGUEZ <jeisonrogo@gmail.com>
2024-03-18 00:12:13 +0000

    injectable

---
+++ lib/base/config/injectable_config.dart
@@ -0,0 +1,12 @@
+import "package:fidelity/base/config/injectable_config.config.dart";
+import "package:get_it/get_it.dart";
+import "package:injectable/injectable.dart";
+
+final GetIt getIt = GetIt.instance;
+
+@InjectableInit(
+  initializerName: "init", // default
+  preferRelativeImports: true, // default
+  asExtension: true, // default
+)
+void configureDependencies() => getIt.init();
